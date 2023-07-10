9638b22a502043c2bc70583a6f6c4e4c09b9e76a
Katie Kermode <kermodekatie@gmail.com>
2023-07-10 10:30:00 +0100

    feat: improve nav bar design

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,28 @@
 import '../css/style.css'
 import '../css/form.css'
+import { useState } from 'react'
 import Head from 'next/head'
 import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
+  const [showLinks, setShowLinks] = useState(false);
+
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
   return (
     <>
       <Head>
         <title>Train 11+ App</title>
       </Head>
 
-      <div className="top-bar">
-        <div className="navbar">
+      <div className="navbar">
+        <div className="app-title">Train 11+</div>
+        <div className="navbar-toggle" onClick={toggleLinks}>
+          <span class="toggle-icon"></span>
+        </div>
+        <div className={`navbar-links ${showLinks ? "show" : ""}`}>
           <Link href="/">Home</Link>
           <Link href="/new">Add Word</Link>
           <Link href="/import">Import Words</Link>
@@ -24,7 +35,7 @@ function MyApp({ Component, pageProps }) {
         ></img>
         
       </div>
-      <div className="app-title">Train 11+</div>
+     
       <div className="grid wrapper">
         <Component {...pageProps} />
       </div>
